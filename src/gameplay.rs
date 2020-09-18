e300c9ffcf8fa6f50af3deee5d1fa4edeacb28bb
Pierre <pseguin2011@protonmail.com>
2020-09-18 15:56:51 -0400

    Refactor - Removed generic card game logic from the game rules logic Extracted the card game engine and imported it as a dependency. Changed all references to the card game engine

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -1,6 +1,7 @@
-use crate::deck::{Card, Deck, DeckType};
-use crate::error::CardGameError;
-use crate::player::{Partners, Player};
+use card_game_engine::deck::{Card, CardSuit, CardValue, Deck, DeckType};
+use card_game_engine::error::CardGameError;
+use card_game_engine::player::Player;
+use crate::player::Partners;
 
 use std::collections::HashMap;
 
@@ -82,7 +83,9 @@ impl Game {
                 if let (Some(player_index_of_partner), Some(partner_index)) = (self.player_index_of_partner(player), self.partners_index(player)) {
                     if self.player_can_open(&hand, true, &self.players[player_index_of_partner]) {
                         self.partners[partner_index].add_points(hand);
-                        self.partners[partner_index].add_points(vec![self.deck.pop_top_discarded_card()]);
+                        if let Some(top_card) = self.deck.pop_top_discarded_card() {
+                            self.partners[partner_index].add_points(vec![top_card]);
+                        }
                         self.players[player].hand.extend(self.deck.take_discard_pile());
                     }
                 }
@@ -120,25 +123,26 @@ impl Game {
     /// This method is missing the addition of wild cards, opening with the top card on the discard pile,
     /// opening if the partner has already opened (Only requiring 1 set of 3 in that case)
     fn player_can_open<'b> (&self, player_cards: &[Card], with_discard_deck: bool, _partner: &'b Player) -> bool {
-        let mut card_sets: HashMap<String, usize> = HashMap::new();
+        let mut card_sets: HashMap<CardValue, usize> = HashMap::new();
         for card in player_cards {
             match card_sets.get_mut(&card.value) {
                 Some(v) =>  *v += 1,
-                None => { card_sets.insert(card.value.clone(), 1); },
+                None => { card_sets.insert(card.value, 1); },
             }
         }
 
         if with_discard_deck {
-            let top_card = self.deck.peek_top_discarded_card();
-            match card_sets.get_mut(&top_card.value) {
-                Some(v) =>  *v += 1,
-                None => { card_sets.insert(top_card.value.clone(), 1); },
+            if let Some(top_card) = self.deck.peek_top_discarded_card() {
+                match card_sets.get_mut(&top_card.value) {
+                    Some(v) =>  *v += 1,
+                    None => { card_sets.insert(top_card.value.clone(), 1); },
+                }
             }
         }
 
         // two and jokers cannot be counted towards an opening move
-        card_sets.remove("Two");
-        card_sets.remove("Joker");
+        card_sets.remove(&CardValue::Two);
+        card_sets.remove(&CardValue::Joker);
 
         let full_set_iterator = card_sets.iter();
         let sets_of_three_or_more = full_set_iterator
@@ -150,9 +154,10 @@ impl Game {
         if sets_of_three_or_more >= 3 {
             return true;
         }
-
-        if let (2, Some(2)) = (sets_of_three_or_more, card_sets.get(&self.deck.peek_top_discarded_card().value)) {
-            return true;
+        if let Some(card_value) = self.deck.peek_top_discarded_card() {
+            if let (2, Some(2)) = (sets_of_three_or_more, card_sets.get(&card_value.value)) {
+                return true;
+            }
         }
 
         false
@@ -181,4 +186,165 @@ impl Game {
         None
     }
     
+}
+
+#[test]
+fn player_a_open() {
+    let mut deck = Deck::new(DeckType::WithJokers);
+    let players = [
+        Player::new("Player 1", vec![
+            Card {value: CardValue::Ace, suit: CardSuit::Clubs},
+            Card {value: CardValue::Ace, suit: CardSuit::Spades},
+            Card {value: CardValue::Ace, suit: CardSuit::Diamonds},
+
+            Card {value: CardValue::Three, suit: CardSuit::Clubs},
+            Card {value: CardValue::Three, suit: CardSuit::Spades},
+            Card {value: CardValue::Three, suit: CardSuit::Diamonds},
+
+            Card {value: CardValue::Eight, suit: CardSuit::Clubs},
+            Card {value: CardValue::Eight, suit: CardSuit::Spades},
+            Card {value: CardValue::Eight, suit: CardSuit::Diamonds},
+            
+        ]),
+        Player::new("Player 2", vec![]),
+        Player::new("Player 3", vec![]),
+        Player::new("Player 4", vec![]),
+    ];
+
+    let partners = [
+        Partners::new(0, 1),
+        Partners::new(2,3),
+    ];
+    
+    let game = Game {
+        players,
+        partners,
+        deck,
+    };
+
+    assert!(game.player_can_open(&game.players[0].hand, false, &game.players[1]));
+}
+
+#[test]
+fn player_b_open() {
+    let mut deck = Deck::new(DeckType::WithJokers);
+    let mut players = [
+        Player::new("Player 1", vec![
+            Card {value: CardValue::Seven, suit: CardSuit::Clubs},
+            Card {value: CardValue::Seven, suit: CardSuit::Spades},
+            Card {value: CardValue::Seven, suit: CardSuit::Diamonds}
+        ]),
+        Player::new("Player 2", vec![
+            Card {value: CardValue::Ace, suit: CardSuit::Clubs},
+            Card {value: CardValue::Ace, suit: CardSuit::Spades},
+            Card {value: CardValue::Ace, suit: CardSuit::Diamonds},
+
+            Card {value: CardValue::Five, suit: CardSuit::Clubs},
+            Card {value: CardValue::Five, suit: CardSuit::Spades},
+            Card {value: CardValue::Five, suit: CardSuit::Diamonds},
+
+            Card {value: CardValue::Four, suit: CardSuit::Clubs},
+            Card {value: CardValue::Four, suit: CardSuit::Spades},
+            Card {value: CardValue::Four, suit: CardSuit::Diamonds},
+        ]),
+        Player::new("Player 3", vec![]),
+        Player::new("Player 4", vec![]),
+    ];
+
+    let mut partners = [
+        Partners::new(0, 1),
+        Partners::new(2,3),
+    ];
+    
+    partners[0].update_status(1);
+    partners[0].add_points(std::mem::take(&mut players[1].hand));
+
+    let game = Game {
+        players,
+        partners,
+        deck,
+    };
+
+    assert!(game.player_can_open(&game.players[0].hand, false, &game.players[1]));
+}
+
+#[test]
+fn player_a_open_with_discard() {
+    let mut deck = Deck::new(DeckType::WithJokers);
+    let players = [
+        Player::new("Player 1", vec![
+            Card {value: CardValue::Ace, suit: CardSuit::Spades},
+            Card {value: CardValue::Ace, suit: CardSuit::Hearts},
+            Card {value: CardValue::Ace, suit: CardSuit::Diamonds},
+
+            Card {value: CardValue::Three, suit: CardSuit::Spades},
+            Card {value: CardValue::Three, suit: CardSuit::Hearts},
+            Card {value: CardValue::Three, suit: CardSuit::Diamonds},
+
+            Card {value: CardValue::Ten, suit: CardSuit::Hearts},
+            Card {value: CardValue::Ten, suit: CardSuit::Diamonds},
+        ]),
+        Player::new("Player 2", vec![]),
+        Player::new("Player 3", vec![]),
+        Player::new("Player 4", vec![]),
+    ];
+    
+    deck.discard_card(
+        Card {value: CardValue::Four, suit: CardSuit::Spades});
+
+    let partners = [
+        Partners::new(0, 1),
+        Partners::new(2,3),
+    ];
+    
+    let game = Game {
+        players,
+        partners,
+        deck,
+    };
+
+    assert!(game.player_can_open(&game.players[0].hand, true, &game.players[1]));
+}
+
+#[test]
+fn player_b_open_with_discard() {
+    let mut deck = Deck::new(DeckType::WithJokers);
+    let mut players = [
+        Player::new("Player 1", vec![
+            Card {value: CardValue::Seven, suit: CardSuit::Diamonds},
+            Card {value: CardValue::Seven, suit: CardSuit::Hearts},
+        ]),
+        Player::new("Player 2", vec![
+            Card {value: CardValue::Ace, suit: CardSuit::Spades},
+            Card {value: CardValue::Ace, suit: CardSuit::Hearts},
+            Card {value: CardValue::Ace, suit: CardSuit::Diamonds},
+            Card {value: CardValue::Three, suit: CardSuit::Spades},
+            Card {value: CardValue::Three, suit: CardSuit::Hearts},
+            Card {value: CardValue::Three, suit: CardSuit::Diamonds},
+            Card {value: CardValue::Four, suit: CardSuit::Spades},
+            Card {value: CardValue::Four, suit: CardSuit::Hearts},
+            Card {value: CardValue::Four, suit: CardSuit::Diamonds},
+        ]),
+        Player::new("Player 3", vec![]),
+        Player::new("Player 4", vec![]),
+    ];
+    
+    deck.discard_card(
+        Card {value: CardValue::Seven, suit: CardSuit::Diamonds});
+
+    let mut partners = [
+        Partners::new(0, 1),
+        Partners::new(2,3),
+    ];
+    
+    partners[0].update_status(1);
+    partners[0].add_points(std::mem::take(&mut players[1].hand));
+
+    let game = Game {
+        players,
+        partners,
+        deck,
+    };
+
+    assert!(game.player_can_open(&game.players[0].hand, true, &game.players[1]));
 }
--- src/player.rs
+++ src/player.rs
@@ -1,31 +1,5 @@
-use crate::deck::Card;
-
+use card_game_engine::deck::{CardValue, Card};
 use std::collections::HashMap;
-
-#[derive(Debug)]
-pub struct Player {
-    name: String,
-    pub hand: Vec<Card>,
-    pub has_opened: bool,
-}
-
-impl Player {
-    pub fn new<S: Into<String>>(name: S, hand: Vec<Card>) -> Self {
-        Player {
-            name: name.into(),
-            hand,
-            has_opened: false,
-        }
-    }
-
-    pub fn add_card_to_hand(&mut self, card: Card) {
-        self.hand.push(card);
-    }
-
-    pub fn play_card_from_hand(&mut self, index: usize) -> Card {
-        self.hand.remove(index)
-    }
-}
 
 #[derive(Clone, Copy, Debug)]
 enum TeamOpenStatus {
@@ -45,7 +19,7 @@ pub enum PlayerAction {
 pub struct Partners {
     player_a_index: usize,
     player_b_index: usize,
-    points_deck: HashMap<String, Vec<Card>>,
+    points_deck: HashMap<CardValue, Vec<Card>>,
     status: TeamOpenStatus,
 }
 
@@ -63,7 +37,7 @@ impl Partners {
         for card in cards {
             match self.points_deck.get_mut(&card.value) {
                 Some(v) => v.push(card),
-                None => { self.points_deck.insert(card.value.clone(), vec![card]); },
+                None => { self.points_deck.insert(card.value, vec![card]); },
             }
         }
     }
